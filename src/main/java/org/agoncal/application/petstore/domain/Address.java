007505f461321634b7a6c2017012082094d33849
Maikon Alexander de Barros Dias <maikonalexander.mabd@gmail.com>
2022-04-29 17:46:41 -0300

    anotação @Getter no Address.java

--- src/main/java/org/agoncal/application/petstore/domain/Address.java
+++ src/main/java/org/agoncal/application/petstore/domain/Address.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.Getter;
 
 /**
  * @author Antonio Goncalves
@@ -21,21 +22,21 @@ public class Address {
     @Column(nullable = false)
     @NotNull
     @Size(min = 5, max = 50)
-    private String street1;
-    private String street2;
+    @Getter private String street1;
+    @Getter private String street2;
     @Column(nullable = false, length = 100)
     @NotNull
     @Size(min = 2, max = 50)
-    private String city;
-    private String state;
+    @Getter private String city;
+    @Getter private String state;
     @Column(name = "zip_code", nullable = false, length = 10)
     @NotNull
     @Size(min = 1, max = 10)
-    private String zipcode;
+    @Getter private String zipcode;
     @Column(nullable = false, length = 50)
     @NotNull
     @Size(min = 2, max = 50)
-    private String country; // TODO use an enum
+    @Getter private String country; // TODO use an enum
 
     // ======================================
     // =            Constructors            =
@@ -55,48 +56,24 @@ public class Address {
     // =         Getters & setters          =
     // ======================================
 
-    public String getStreet1() {
-        return street1;
-    }
-
     public void setStreet1(String street1) {
         this.street1 = street1;
-    }
-
-    public String getStreet2() {
-        return street2;
     }
 
     public void setStreet2(String street2) {
         this.street2 = street2;
     }
 
-    public String getCity() {
-        return city;
-    }
-
     public void setCity(String city) {
         this.city = city;
-    }
-
-    public String getState() {
-        return state;
     }
 
     public void setState(String state) {
         this.state = state;
     }
 
-    public String getZipcode() {
-        return zipcode;
-    }
-
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
-    }
-
-    public String getCountry() {
-        return country;
     }
 
     public void setCountry(String country) {
