e4545eb7cf3470a4acc70ea1fcee1db28146d5f3
Maikon Alexander de Barros Dias <maikonalexander.mabd@gmail.com>
2022-05-01 14:10:43 -0300

    anotação @Getter no CreditCard.java

--- src/main/java/org/agoncal/application/petstore/domain/CreditCard.java
+++ src/main/java/org/agoncal/application/petstore/domain/CreditCard.java
@@ -6,7 +6,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
+import lombok.Getter;
 /**
  * @author Antonio Goncalves
  *         http://www.antoniogoncalves.org
@@ -23,15 +23,15 @@ public class CreditCard {
     @Column(name = "credit_card_number", length = 30)
     @NotNull
     @Size(min = 1, max = 30)
-    private String creditCardNumber;
+    @Getter private String creditCardNumber;
     @Column(name = "credit_card_type")
     @NotNull
     @Enumerated(EnumType.STRING)
-    private CreditCardType creditCardType;
+    @Getter private CreditCardType creditCardType;
     @Column(name = "credit_card_expiry_date", length = 5)
     @NotNull
     @Size(min = 1, max = 5)
-    private String creditCardExpDate;
+    @Getter private String creditCardExpDate;
 
     // ======================================
     // =            Constructors            =
@@ -49,25 +49,12 @@ public class CreditCard {
     // ======================================
     // =         Getters & setters          =
     // ======================================
-
-    public String getCreditCardNumber() {
-        return creditCardNumber;
-    }
-
     public void setCreditCardNumber(String creditCardNumber) {
         this.creditCardNumber = creditCardNumber;
     }
 
-    public CreditCardType getCreditCardType() {
-        return creditCardType;
-    }
-
     public void setCreditCardType(CreditCardType creditCardType) {
         this.creditCardType = creditCardType;
-    }
-
-    public String getCreditCardExpDate() {
-        return creditCardExpDate;
     }
 
     public void setCreditCardExpDate(String creditCardExpDate) {
