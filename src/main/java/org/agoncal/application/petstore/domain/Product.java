1ce4e8b6d207039d4cd274640384fd6ef39df39f
Maikon Alexander de Barros Dias <maikonalexander.mabd@gmail.com>
2022-05-02 23:08:04 -0300

    adicionando @Setter e AccessLevel no Product.java

--- src/main/java/org/agoncal/application/petstore/domain/Product.java
+++ src/main/java/org/agoncal/application/petstore/domain/Product.java
@@ -8,6 +8,8 @@ import javax.xml.bind.annotation.XmlTransient;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
+import lombok.AccessLevel;
+import lombok.Setter;
 
 /**
  * @author Antonio Goncalves
@@ -24,6 +26,7 @@ import lombok.Getter;
 })
 @XmlRootElement
 @Getter
+@Setter
 public class Product {
 
     // ======================================
@@ -32,7 +35,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Setter(AccessLevel.NONE) private Long id;
     @Column(nullable = false, length = 30)
     @NotNull
     @Size(min = 1, max = 30)
@@ -72,26 +75,10 @@ public class Product {
     // =         Getters & setters          =
     // ======================================
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
     public void addItem(Item item) {
         if (items == null)
             items = new ArrayList<Item>();
         items.add(item);
-    }
-
-    public void setItems(List<Item> items) {
-        this.items = items;
     }
 
     // ======================================
